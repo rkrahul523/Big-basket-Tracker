7d2c314406abee45f36002f3fdf6b67650277495
rkrahul523 <57534781+rkrahul523@users.noreply.github.com>
2022-12-26 17:35:52 +0530

    added  scenarios

--- index.js
+++ index.js
@@ -42,11 +42,10 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
 
-
 let socketIO = require('socket.io');
 // let io = socketIO(server, { origins: '*:*'});
 
- const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer');
 const USERNAME_SELECTOR = '#login > login > div > form > div:nth-child(1) > div > label';
 const obj = { mob: '7979835401', otp: '123456', iterate: 4 }
 const inputfield = '#root > div > form > div > div > input';
@@ -61,9 +60,9 @@ const cart = '#navbar-main > div > bigbasket-cart-template > div > div.hidden-md
 const checkoutselector = ' #checkout > p'
 const noslotsavailable = '#noSlotModal > div > div.slotmodal-footer > button'
 
-const downarrow='#headerControllerId > header > div > div > div > div > ul > li:nth-child(2) > div > a > span > span.arrow-marker'
-const slottext='#headerControllerId > header > div > div > div > div > ul > li:nth-child(2) > div > div > div.other-address.ng-scope > ul > li > a > div'
-const slotavailabletext='#headerControllerId > header > div > div > div > div > ul > li:nth-child(2) > div > div > div.other-address.ng-scope > ul > li > a > div > div > div'
+const downarrow = '#headerControllerId > header > div > div > div > div > ul > li:nth-child(2) > div > a > span > span.arrow-marker'
+const slottext = '#headerControllerId > header > div > div > div > div > ul > li:nth-child(2) > div > div > div.other-address.ng-scope > ul > li > a > div'
+const slotavailabletext = '#headerControllerId > header > div > div > div > div > ul > li:nth-child(2) > div > div > div.other-address.ng-scope > ul > li > a > div > div > div'
 
 
 
@@ -74,191 +73,187 @@ const accountSid = 'ACe13dbe41143f6de109884291bbfb2817';
 const authToken = 'cafad106119d0afe827d6b5f06f32c93';
 const client = require('twilio')(accountSid, authToken);
 
+const details = {
+   firstName: 'Rohit Kumar',
+   email: 'rkrah523@gmail.com',
+   pin: '800006',
+   house: 'at Nalanda House ,golakpur rani ghat',
+   area: 'MOB 7903328849',
+   landmark: 'near NIT gate No. 2 ',
+   mob: '7979835402',
+   otp: '123456'
+
+
+}
+
+
+
 
 // app.get('/', function (req, res) {
-   
+
 //    res.send( "api started working" )
 // })
 
 
+const RECIEVE_FOR_FREE = `body > div.wrap > div.benefits-section > div.content > a`
+const FIRST_NAME = '#first_name'
+
+const EMAIL = '#email';
+const PINCODE = '#pincode';
+const HOUSE = '#addessline1';
+const AREA = '#addessline2';
+const LANDMARK = '#landmark';
+const CONTACT = '#contact_no';
+const OUTSIDE_CLICK = '#reg_details_form > div.form > div.block.otp-box';
+const ERROR_SELECTOR = '#number_error_msg';
+const AGREE_1 = '#reg_details_form > div.form > div.terms-message > div.terms-message-checkbox > label:nth-child(1) > div.checkbox';
+const AGREE_2 = '#reg_details_form > div.form > div.terms-message > div.terms-message-checkbox > label:nth-child(3) > div.checkbox'
+const CONFIRM_OTP = '#reg_details_form > div.form > div.access-point > button'
+const VERIFY_OTP = 'body > div.wrap > div.registration-section > div.data-content > div > div.verification-info > div.otp-verification > div.form > div.access-point > div.button.contribute.primary.vertify-btn';
+const CODEBOX_1 = '#codeBox1'
+const SKIP_SELECTOR = '#donate_form > div > div.access-point > div'
+
+const INVALID_OTP = "#otp-error-msg"
+// const EMAIL= '#email';
+
 // endpoint to post otp
 app.post('/postotp', function (req, res) {
-   obj.otp = req.body.otp;
+   details.otp = req.body.otp;
    res.send({ status: "success" })
 })
 
 // endpoint to post mob nop. and no. of iteration to execute
 app.post('/postmob', function (req, res) {
-   obj.mob = req.body.mob;
-   obj.iterate = parseInt(req.body.iterate);
+   details.mob = req.body.mob;
+   // obj.iterate = parseInt(req.body.iterate);
    res.send({ status: "success" })
 })
-const RECIEVE_FOR_FREE=`body > div.wrap > div.benefits-section > div.content > a`
+
+
+app.get('/startOrder1', async function (req, res) {
+   io.emit('waychat', 'started');
+
+})
 
 
 // endpoint to the p[rescribed slot if available
-app.get('/getslot', async function (req, res) {
-   console.log("get slot called")
-
-   async function getPic() {
-      //comment headless
-     
-const browser = await puppeteer.launch({ headless: false, args:['--no-sandbox'] })
-      const page = await browser.newPage();
-      await page.setViewport({
-         width: 1100,
-         height: 700,
-     });
-      await page.goto('https://www.bigbasket.com/auth/login/');
-      await page.waitFor(10000)
-      await page.click(USERNAME_SELECTOR);
-
-
-console.log("entering mob no.")
-      await page.waitFor(3000)
-      await page.type(USERNAME_SELECTOR, obj.mob, { delay: 2 });
-      await page.waitFor(5000)
-      // await page.keyboard.type(obj.mob);
-      await page.click(loginbutton);
-
-      await page.waitFor(26000)
-      console.log("entering otp")
-      await page.type(inputfieldsforOTP, obj.otp, { delay: 3 })
-      await page.waitFor(2000)
-      await page.click(loginbuttonfinal);
-      await page.waitFor(10000)
-      console.log("login success")
-
-  var counting=1;
-      while (obj.iterate--) {
-         console.log(`checking for slots for ${counting++}`)
-         await page.reload({waitUntil: ["load","networkidle2"], timeout: 0})
-      await page.waitFor(10000)
-      await page.click(downarrow);
-      await page.waitFor(7000)
-
-      if (await page.$(slotavailabletext) !== null) {
-          console.log("slot text available")
-         const element = await page.$(slotavailabletext);
-         const text = await page.evaluate(element => element.textContent, element);
-
-         if(text== "All Slots Full. Please Try Again Later")
-         {
-
-         }
-         else{
-            client.messages
-            .create({
-               body: 'Hurray Rahul!!slot is available order at earliest',
-               from: '+18156688642',
-               to: rahulno
-            })
-            .then(message => console.log(message, message.sid));
-            await browser.close();
-            res.send({ status: 'success', time:`order at earliest as slot is available at ${new Date()}`  })
-         }
-         
-      }
-      else
-      {
-         await page.screenshot({ path: 'google.png' });
-         client.messages
-            .create({
-               body: 'Hurray Rahul!!chances of  slot is available',
-               from: '+18156688642',
-               to: rahulno
-            })
-            .then(message => console.log(message, message.sid));
-            await browser.close();
-         res.send({ status: 'success', time:`chances of  slot is available at ${new Date()}`  })
-      }
-
-      if (obj.iterate == 0) {
-         await browser.close();
-         console.log("exiting application")
-         client.messages
-            .create({
-               body: 'Sorry Rahul!! Hard luck slot not available',
-               from: '+18156688642',
-               to: rahulno
-            })
-            .then(message => console.log(message, message.sid));
-         res.send({ status: 'failed', time:`Hard luck  slot not available at ${new Date()}` })
-        
-      }
-
-   }
-
-
-}
-
-    
-
-
-
-   
-
-
-   
-
-   await getPic();
-
-
-
-});
-
-
-// endpoint to the p[rescribed slot if available
-app.get('/', async function (req, res) {
+app.get('/startOrder', async function (req, res) {
    console.log("api place order hitted")
 
    async function getPic() {
       //comment headless
-     
-     const browser = await puppeteer.launch({ headless: true, args:['--no-sandbox'] })
+
+      const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] })
       const page = await browser.newPage();
       await page.setViewport({
          width: 1100,
          height: 700,
-     });
+      });
       await page.goto('https://mahashivarathri.org/en/rudraksha-diksha');
       await page.waitFor(10000)
       await page.click(RECIEVE_FOR_FREE);
       await page.waitFor(2000)
-      await page.screenshot({path: 'example.png'});
-      await browser.close();
+      //  await page.screenshot({ path: 'example.png' });
+      // res.write("foo");
 
-      res.send({status: 200})
-// console.log("entering mob no.")
-//       await page.waitFor(3000)
-//       await page.type(USERNAME_SELECTOR, obj.mob, { delay: 2 });
-//       await page.waitFor(5000)
-//       // await page.keyboard.type(obj.mob);
-//       await page.click(loginbutton);
+      io.emit('waychat', `Page Opens ,with ${details.mob}`);
 
-//       await page.waitFor(26000)
-//       console.log("entering otp")
-//       await page.type(inputfieldsforOTP, obj.otp, { delay: 3 })
-//       await page.waitFor(2000)
-//       await page.click(loginbuttonfinal);
-//       await page.waitFor(10000)
-//       console.log("login success")
-
-  
-
-
-}
-
-    
+      await page.type(FIRST_NAME, details.firstName, { delay: 100 });
+      await page.type(EMAIL, details.email, { delay: 10 });
+      await page.type(PINCODE, details.pin, { delay: 1000 });
+      // res.write("bar");
+      await page.type(HOUSE, details.house, { delay: 100 });
+      await page.type(AREA, details.area, { delay: 100 });
+      await page.type(LANDMARK, details.landmark, { delay: 100 });
+      await page.type(CONTACT, details.mob, { delay: 1000 });
 
 
 
-   
+      await page.click(OUTSIDE_CLICK);
+      await page.waitFor(500)
+
+      if (await page.$(ERROR_SELECTOR) !== null) {
+
+         const element = await page.$(ERROR_SELECTOR);
+         const text = await page.evaluate(element => element.textContent, element);
+         if (text == "!First-time registrants only") {
+            console.log("found")
+            io.emit('waychat', `DUPLICATE USER`);
+            res.send({ status: false, message: "duplicate user" })
+         }
+         else {
+            await page.click(AGREE_1);
+            await page.waitFor(100)
+            await page.click(AGREE_2);
+            await page.waitFor(100)
+            await page.click(CONFIRM_OTP);
+            io.emit('waychat', `WAITING FOR OTP`);
+            await page.waitFor(30000)
+
+            io.emit('waychat', `Entering Otp: ${details.otp}`);
+            await page.type(CODEBOX_1, details.otp, { delay: 1000 });
+            await page.click(VERIFY_OTP);
+            await page.waitFor(10000)
+          
+            if (await page.$(INVALID_OTP) !== null) {
+
+               const element = await page.$(INVALID_OTP);
+               const text = await page.evaluate(element => element.textContent, element);
+               console.log("invalid",text)
+
+               if (text == "Invalid OTP.") {
+                  io.emit('waychat', `WRONG OTP`);
+                  await browser.close();
+                  res.send({ status: false, message: "Wrong OTP" })
+               }
+               else {
+                  io.emit('waychat', `OTP Verified`);
+                  await page.waitFor(5000)
+
+                 
+                  await page.click(SKIP_SELECTOR);
+
+                  await page.waitFor(3000)
+                  // await page.screenshot({ path: 'example.png' });
+                  //await page.waitFor(4000)
+                  await browser.close();
+                  io.emit('waychat', `SUCCESSFULY PLACED`);
+                  res.send({ status: true, message: "successfully placed" })
+               }
+
+            }
+            else{
+               {
+                  io.emit('waychat', `OTP Verified`);
+                  await page.waitFor(5000)
+                  await page.click(SKIP_SELECTOR);
+
+                  await page.waitFor(3000)
+                  // await page.screenshot({ path: 'example.png' });
+                  //await page.waitFor(4000)
+                  await browser.close();
+                  io.emit('waychat', `SUCCESSFULY PLACED`);
+                  res.send({ status: true, message: "successfully placed" })
+               }
+            }
+
+         }
+      }
 
 
-   
 
-   await getPic();
+   }
+
+
+
+
+
+
+   await getPic().catch((error) => {
+     // await browser.close();
+      res.send({ status: false, message: "Some Error Occured" })
+
+   });;
 
 
 
@@ -269,15 +264,15 @@ io.on('connection', (socket) => {
    console.log('a user connected');
    socket.on('waychat', (message) => {
       console.log(message);
-      io.emit(  'waychat', message);
-    });
-    
+      io.emit('waychat', message);
+   });
+
 });
 
 io.on('waychat', (message) => {
-   console.log("message is",message)
+   console.log("message is", message)
    io.emit(message);
- });
+});
 
 
 
