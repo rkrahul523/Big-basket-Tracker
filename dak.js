e94d60bcc26a53d2ad7ec3e67815ef19f6b5c5b1
rkrahul523 <57534781+rkrahul523@users.noreply.github.com>
2024-06-02 18:07:42 +0530

    creds path updated

--- dak.js
+++ dak.js
@@ -1,6 +1,6 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require("./etc/secrets/creds.json");
+var serviceAccount = require("./creds.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
